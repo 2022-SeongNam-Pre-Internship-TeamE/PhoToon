f4d7d911edf3f2a963805c4809c58fa294f33cc8
KoneJ <kbvv67890@gmail.com>
2022-09-07 16:15:41 +0900

    style : 로그인 페이지 1차 구성

--- frontend/src/components/Auth/index.js
+++ frontend/src/components/Auth/index.js
@@ -1,2 +1,4 @@
 export { default as LoginWrapper } from './LoginWrapper';
 export { default as JoinWrapper } from './JoinWrapper';
+export { default as InputWithLabel } from './InputWithLabel';
+export { default as RightAlignedLink } from './RightAlignedLink';
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import { LoginWrapper } from '../components/Auth';
+import { LoginWrapper, InputWithLabel, RightAlignedLink } from '../components/Auth';
+import MainButton from "../components/MainButton";
 
 class Login extends Component {
     
     render() {
         return (
             <LoginWrapper>
-        
+                <InputWithLabel name="email" placeholder="이메일"/>
+                <InputWithLabel name="password" placeholder="비밀번호" type="password"/>
+                <div style= {{marginTop : '35px'}}>
+                    <MainButton text="LOGIN"></MainButton>
+                </div>
+                <RightAlignedLink to="/Join">회원가입</RightAlignedLink>
             </LoginWrapper>
+            
         );
     }
 }
