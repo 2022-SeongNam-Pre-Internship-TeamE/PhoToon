b3d5270504f951d83c4fc0531c633d04cf1113b0
KoneJ <kbvv67890@gmail.com>
2022-09-09 02:47:08 +0900

    style : Mypage 모달 창 퍼블리싱 1차 구현

--- frontend/src/pages/Mypage.js
+++ frontend/src/pages/Mypage.js
@@ -1,15 +1,20 @@
 import MypageModal from '../components/MypageModal';
 import React from "react";
-import Button from 'react-bootstrap/Button';
+import style from "./MainPage.module.css";
 
 export default function  Mypage() {
       const [modalShow, setModalShow] = React.useState(false);
     
       return (
         <>
-          <Button variant="primary" onClick={() => setModalShow(true)}>
-            Launch vertically centered modal
-          </Button>
+        <div className={`${style.box1}`}>
+        <img className={`${style.logo}`} src="images/logo.png" alt="logo" />
+      </div>
+          <div  onClick={() => setModalShow(true)}>
+                {/* 임시로 모달창 넘어가는 이미지 삽입하였음. */}
+          <h2 style= {{textAlign : 'center' , marginTop : '90px'}}>아래 로고 클릭해서 모달 창 열기, 추후 이미지 받아오면 onClick 설정</h2>
+          <img className={`${style.logo}`} src="images/logo.png" alt="logo" />
+          </div>
     
           <MypageModal
             show={modalShow}
