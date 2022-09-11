804fbf9306c381fba5ebd0fb006bf295abfd6fc4
baejunil <bjo6300@naver.com>
2022-09-12 00:41:36 +0900

    chore : url endpoint 변경

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('photoon.urls'))
+    path('api/v1/', include('photoon.urls'))
 ]
--- backend/photoon/urls.py
+++ backend/photoon/urls.py
@@ -4,15 +4,17 @@ from rest_framework import routers
 from rest_framework_simplejwt.views import TokenRefreshView
 
 router = routers.DefaultRouter()
-router.register(r'api/v1/origins', OriginViewset)
-router.register(r'api/v1/results', ResultViewset)
-router.register(r'api/v1/styles', StyleViewset)
-router.register(r'api/v1/speech-bubbles', SpeechViewset)
-router.register(r'api/v1/list', UserViewSet) # header에 Authorize : Bearer <acess 토큰>
+router.register(r'origins', OriginViewset)
+router.register(r'results', ResultViewset)
+router.register(r'styles', StyleViewset)
+router.register(r'speech-bubbles', SpeechViewset)
+router.register(r'users', UserViewSet) # header에 Authorize : Bearer <access 토큰> ### 테스트용
 
+# postman으로 router에 있는 것을 테스트할 때 '/' 유무 안 중요함
+# postman으로 urlpatterns에 있는 것을 테스트할 때 '/' 유무 중요함
 urlpatterns = [
     path('', include(router.urls)),
-    path("api/v1/register/", RegisterAPIView.as_view()), # post - 회원가입
-    path("api/v1/auth/", AuthAPIView.as_view()), # post - 로그인
-    path("api/v1/auth/refresh/", TokenRefreshView.as_view()), # jwt 토큰 재발급
+    path("join", RegisterAPIView.as_view()), # post - 회원가입
+    path("auth", AuthAPIView.as_view()), # post - 로그인
+    path("auth/refresh", TokenRefreshView.as_view()), # jwt 토큰 재발급
 ]
