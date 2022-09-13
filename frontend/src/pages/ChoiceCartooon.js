30515642d97087ee69d3f1e07f2843bd6ddc86d3
minjilee <budlalswl1129@gmail.com>
2022-09-13 15:21:56 +0900

    chore: cartoonchoice 로고변경

--- frontend/src/pages/ChoiceCartooon.js
+++ frontend/src/pages/ChoiceCartooon.js
@@ -7,9 +7,11 @@ import CartoonSlider from "../components/CartoonSlider";
 export default function Start() {
   return (
     <>
-      <UserPageButton />
-      <div className={`${style.box1}`} style={{ height: "10rem" }}>
-        <img className={`${style.logo}`} src="images/logo.png" alt="logo" />
+      <div className="flex">
+        <div className={`${style.box1}`}>
+          <img className={`${style.logo2}`} src="images/logo.png" alt="logo" />
+        </div>
+        <UserPageButton className="float-left" />
       </div>
       <CartoonSlider></CartoonSlider>
       <MoveButton url1="/start" url2="" style1="w-12" style2="w-12" />
