70b1215a694e532df0e6c56e71805cec6937ce66
KoneJ <kbvv67890@gmail.com>
2022-09-16 01:51:22 +0900

    feat : 로그인 회원가입 통신 미완성, (버튼 수정필요)

--- frontend/src/pages/Join.js
+++ frontend/src/pages/Join.js
@@ -1,27 +1,94 @@
-import React from "react";
+import React, { useState }from "react";
 import { JoinWrapper, JoinInput } from "../components/Auth";
-import MainButton from "../components/MainButton";
 import style from "./MainPage.module.css";
-import { Link } from "react-router-dom";
+import axios from 'axios';
+
 
 export default function Join() {
+
+  const [email, setEmail] = useState('')
+  const [passwd, setPasswd] = useState('')
+  const [passwdcheck, setPasswdcheck] = useState('')
+  const [errors, setErrors] = useState(false)
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const onChangePwd1 = (e) => {
+    setPasswd(e.target.value)
+  }
+
+  const onChangePwd2 = (e) => {
+    setPasswdcheck(e.target.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const user = {
+      email: email,
+      passwd: passwd,
+      passwdcheck: passwdcheck
+    }
+
+    // 유효성 검사
+    if(passwd !== passwdcheck) {
+      alert('비밀번호와 비밀번호 확인이 일치하지 않습니다')
+      return false
+    }
+
+    axios.post('/api/v1/mall/join', user)
+      .then(res => {
+        if (res.data.key) {
+          localStorage.clear()
+          localStorage.setItem('token', res.data.key)
+          // 사용하려면 App.js에서 /로 라우팅해야 한다
+          window.location.replace('/')
+        } else {
+          setEmail('')
+          setPasswd('')
+          setPasswdcheck('')
+          localStorage.clear()
+          setErrors(true)
+        }
+      })
+      .catch(err => {
+        console.clear()
+        alert('아이디 혹은 비밀번호가 일치하지 않습니다')
+      })
+  }
   return (
     <div className="min-h-screen">
       <div className={`${style.box1}`}>
         <img className={`${style.logo1}`} src="images/logo.png" alt="logo" />
       </div>
       <JoinWrapper>
-        <JoinInput label="E-MAIL" name="email" />
-        <JoinInput label="PASSWORD" name="password" type="password" />
+        <form onSubmit={onSubmit}>
+        <JoinInput 
+        type="email"
+        value={email}
+        onChange={onChangeEmail}
+        required
+        label="E-MAIL" />
+        <JoinInput 
+        type="password"
+        value={passwd}
+        onChange={onChangePwd1}
+        required
+        label="PASSWORD"  />
         <JoinInput
-          label="PASSWORD CHECK"
-          name="password check"
           type="password"
+          value={passwdcheck}
+          onChange={onChangePwd2}
+          required
+          label="PASSWORD CHECK"
         />
         <div style={{ marginTop: "35px" }}>
           {/* 임시로 로그인 페이지 라우팅 */}
-          <MainButton url="/Login" text="SIGN UP"></MainButton>
+          <button url="/Login" value='Signup' >Sign UP</button>
         </div>
+        </form>
       </JoinWrapper>
     </div>
   );
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,22 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { LoginWrapper, LoginInput, RightAlignedLink } from "../components/Auth";
-import MainButton from "../components/MainButton";
 import style from "./MainPage.module.css";
+import axios from 'axios';
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [passwd, setPasswd] = useState('')
+  const [errors, setErrors] = useState(false)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const user = {
+      email: email,
+      passwd: passwd
+    }
+
+    axios.post('/api/v1/auth', user)
+      .then(res => {
+        if (res.data.key) {
+          localStorage.clear()
+          localStorage.setItem('token', res.data.key)
+          // 사용하려면 App.js에서 /로 라우팅해야 한다
+          window.location.replace('/')
+        } else {
+          setEmail('')
+          setPasswd('')
+          localStorage.clear()
+          setErrors(true)
+        }
+      })
+      .catch(err => {
+        console.clear()
+        alert('아이디 또는 비밀번호가 일치하지 않습니다')
+        setEmail('')
+        setPasswd('')
+      })
+  }
+
   return (
     <div className="min-h-screen">
       <div className={`${style.box1}`}>
         <img className={`${style.logo1}`} src="images/logo.png" alt="logo" />
       </div>
-
+      
       <LoginWrapper>
-        <LoginInput name="email" placeholder="E-MAIL" />
-        <LoginInput name="password" placeholder="PASSWORD" type="password" />
+        <form onSubmit={onSubmit}>
+        <LoginInput 
+        type='email'
+        value={email}
+        required
+        onChange={e => setEmail(e.target.value)}
+        placeholder="E-MAIL" />
+        <LoginInput 
+        type='password'
+        value={passwd}
+        required
+        onChange={e => setPasswd(e.target.value)}
+        placeholder="PASSWORD"  />
         <div style={{ marginTop: "35px" }}>
-          <MainButton url="/AfterLogin" text="LOGIN"></MainButton>
+          <button url="/AfterLogin" >Login</button>
         </div>
+        </form>
         <RightAlignedLink to="/Join">SIGN UP →</RightAlignedLink>
+        
       </LoginWrapper>
     </div>
   );
