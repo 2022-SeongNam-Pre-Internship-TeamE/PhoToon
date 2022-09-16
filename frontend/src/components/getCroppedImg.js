36d71b1826819b7ac944acb957d8f0281c66323a
minjilee <budlalswl1129@gmail.com>
2022-09-17 01:57:44 +0900

    feat: 이미지 크롭 기능 추가

---
+++ frontend/src/components/getCroppedImg.js
@@ -0,0 +1,42 @@
+import { Area } from "react-easy-crop";
+export const createImage = (url) =>
+  new Promise((resolve, reject) => {
+    const image = new Image();
+    image.addEventListener("load", () => resolve(image));
+    image.addEventListener("error", (error) => reject(error));
+
+    image.setAttribute("crossOrigin", "anonymous");
+    image.src = url;
+  });
+
+export default async function getCroppedImg(imageSrc, pixelCrop) {
+  const image = await createImage(imageSrc);
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  if (!ctx) {
+    return "";
+  }
+
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  ctx.drawImage(image, 0, 0);
+
+  const data = ctx.getImageData(
+    pixelCrop.x,
+    pixelCrop.y,
+    pixelCrop.width,
+    pixelCrop.height
+  );
+
+  canvas.width = pixelCrop.width;
+  canvas.height = pixelCrop.height;
+
+  ctx.putImageData(data, 0, 0);
+
+  return new Promise((resolve, reject) => {
+    canvas.toBlob((file) => {
+      if (file !== null) resolve(URL.createObjectURL(file));
+    }, "image/jpeg");
+  });
+}
