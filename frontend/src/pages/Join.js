beaebc7957a1e306bed192d84f69a4fc1ccadc1b
KoneJ <kbvv67890@gmail.com>
2022-09-19 22:05:50 +0900

    feat : Auth 페이지 통신

--- frontend/src/pages/Join.js
+++ frontend/src/pages/Join.js
@@ -1,4 +1,5 @@
 import React, { useState }from "react";
+import { useNavigate} from "react-router-dom";
 import { JoinWrapper, JoinInput } from "../components/Auth";
 import style from "./MainPage.module.css";
 import axios from 'axios';
@@ -6,89 +7,98 @@ import axios from 'axios';
 
 export default function Join() {
 
-  const [email, setEmail] = useState('')
-  const [passwd, setPasswd] = useState('')
-  const [passwdcheck, setPasswdcheck] = useState('')
+  
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordcheck, setPasswordcheck] = useState("")
   const [errors, setErrors] = useState(false)
+  const navigate = useNavigate();
 
-  const onChangeEmail = (e) => {
-    setEmail(e.target.value)
-  }
-
-  const onChangePwd1 = (e) => {
-    setPasswd(e.target.value)
-  }
-
-  const onChangePwd2 = (e) => {
-    setPasswdcheck(e.target.value)
-  }
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-
-    const user = {
-      email: email,
-      passwd: passwd,
-      passwdcheck: passwdcheck
+  const user = () => {
+    axios
+  .post('/api/v1/join', {
+    email: email,
+    password: password,
+  })
+  .then(response => {
+    // Handle success.
+    console.log('Well done!');
+    console.log('User profile', response.data.user);
+    console.log('User token', response.data.jwt);
+    localStorage.setItem('token', response.data.jwt);
+    navigate("/login");
+  })
+  .catch(error => {
+    // Handle error.
+    console.log('An error occurred:', error.response);
+  });
     }
 
     // 유효성 검사
-    if(passwd !== passwdcheck) {
-      alert('비밀번호와 비밀번호 확인이 일치하지 않습니다')
-      return false
-    }
+    // if(passwd !== passwdcheck) {
+    //   alert('비밀번호와 비밀번호 확인이 일치하지 않습니다')
+    //   return false
+    // }
 
-    axios.post('/api/v1/mall/join', user)
-      .then(res => {
-        if (res.data.key) {
-          localStorage.clear()
-          localStorage.setItem('token', res.data.key)
-          // 사용하려면 App.js에서 /로 라우팅해야 한다
-          window.location.replace('/')
-        } else {
-          setEmail('')
-          setPasswd('')
-          setPasswdcheck('')
-          localStorage.clear()
-          setErrors(true)
-        }
-      })
-      .catch(err => {
-        console.clear()
-        alert('아이디 혹은 비밀번호가 일치하지 않습니다')
-      })
-  }
+    // axios.post('/api/v1/mall/join', user)
+    //   .then(res => {
+    //     if (res.data.key) {
+    //       localStorage.clear()
+    //       localStorage.setItem('token', res.data.key)
+    //       // 사용하려면 App.js에서 /로 라우팅해야 한다
+    //       window.location.replace('/')
+    //     } else {
+    //       setEmail('')
+    //       setPasswd('')
+    //       setPasswdcheck('')
+    //       localStorage.clear()
+    //       setErrors(true)
+    //     }
+    //   })
+    //   .catch(err => {
+    //     console.clear()
+    //     alert('아이디 혹은 비밀번호가 일치하지 않습니다')
+    //   })
+  
   return (
     <div className="min-h-screen">
       <div className={`${style.box1}`}>
         <img className={`${style.logo1}`} src="images/logo.png" alt="logo" />
       </div>
       <JoinWrapper>
-        <form onSubmit={onSubmit}>
         <JoinInput 
         type="email"
         value={email}
-        onChange={onChangeEmail}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
         required
         label="E-MAIL" />
+
         <JoinInput 
         type="password"
-        value={passwd}
-        onChange={onChangePwd1}
+        value={password}
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
         required
         label="PASSWORD"  />
-        <JoinInput
+
+        {/* <JoinInput
           type="password"
           value={passwdcheck}
-          onChange={onChangePwd2}
+          //onChange={onChangePwd2}
           required
           label="PASSWORD CHECK"
-        />
+        /> */}
+
         <div style={{ marginTop: "35px" }}>
           {/* 임시로 로그인 페이지 라우팅 */}
-          <button url="/Login" value='Signup' >Sign UP</button>
+          <button onClick={() => {
+            user();
+          }}>Sign UP</button>
         </div>
-        </form>
+        
       </JoinWrapper>
     </div>
   );
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,42 +1,79 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { LoginWrapper, LoginInput, RightAlignedLink } from "../components/Auth";
 import style from "./MainPage.module.css";
 import axios from 'axios';
+import { useNavigate} from "react-router-dom";
+
 
 export default function Login() {
-  const [email, setEmail] = useState('')
-  const [passwd, setPasswd] = useState('')
-  const [errors, setErrors] = useState(false)
+const [email, setEmail] = useState("")
+const [password, setPassword] = useState("") 
+const navigate = useNavigate();
 
-  const onSubmit = (e) => {
-    e.preventDefault()
-
-    const user = {
-      email: email,
-      passwd: passwd
-    }
-
-    axios.post('/api/v1/auth', user)
-      .then(res => {
-        if (res.data.key) {
-          localStorage.clear()
-          localStorage.setItem('token', res.data.key)
-          // 사용하려면 App.js에서 /로 라우팅해야 한다
-          window.location.replace('/')
-        } else {
-          setEmail('')
-          setPasswd('')
-          localStorage.clear()
-          setErrors(true)
-        }
-      })
-      .catch(err => {
-        console.clear()
-        alert('아이디 또는 비밀번호가 일치하지 않습니다')
-        setEmail('')
-        setPasswd('')
-      })
+const checkUser = () => {
+  if (email === "" || password === "") {
+    alert("아이디와 비밀번호를 입력해주세요");
+    return;
   }
+}
+
+axios
+  .post('/api/v1/auth', {
+    email: email,
+    password: password,
+  })
+  .then(response => {
+    // Handle success.
+    console.log('Well done!');
+    console.log('User token', response.data.jwt);
+    localStorage.setItem("token" , response.data.jwt);
+    navigate("/afterlogin");
+  })
+  .catch(error => {
+    // Handle error.
+    console.log('An error occurred:', error.response);
+  });
+
+
+//   const [email, setEmail] = useState('')
+//   const [passwd, setPasswd] = useState('')
+//   const [errors, setErrors] = useState(false)
+
+//   const onSubmit = (e) => {
+//     e.preventDefault()
+
+//     const user = {
+//       email: email,
+//       passwd: passwd
+//     }
+
+//     axios.post('/api/v1/auth', user)
+//       .then(res => {
+//         if (res.data.key) {
+//           localStorage.clear()
+//           localStorage.setItem('token', res.data.key)
+//           // 사용하려면 App.js에서 /로 라우팅해야 한다
+//           window.location.replace('/')
+//         } else {
+//           setEmail('')
+//           setPasswd('')
+//           localStorage.clear()
+//           setErrors(true)
+//         }
+//       })
+//       .catch(err => {
+//         console.clear()
+//         alert('아이디 또는 비밀번호가 일치하지 않습니다')
+//         setEmail('')
+//         setPasswd('')
+//       })
+//   }
+
+useEffect(() => {
+  if (localStorage.getItem("token")){
+    navigate("/");
+  }
+}, [])
 
   return (
     <div className="min-h-screen">
@@ -45,23 +82,25 @@ export default function Login() {
       </div>
       
       <LoginWrapper>
-        <form onSubmit={onSubmit}>
         <LoginInput 
         type='email'
         value={email}
         required
-        onChange={e => setEmail(e.target.value)}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
         placeholder="E-MAIL" />
+
         <LoginInput 
         type='password'
-        value={passwd}
+        value={password}
         required
-        onChange={e => setPasswd(e.target.value)}
+        onChange={e => setPassword(e.target.value)}
         placeholder="PASSWORD"  />
+
         <div style={{ marginTop: "35px" }}>
           <button url="/AfterLogin" >Login</button>
         </div>
-        </form>
         <RightAlignedLink to="/Join">SIGN UP →</RightAlignedLink>
         
       </LoginWrapper>
