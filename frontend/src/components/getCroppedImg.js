2dcca591a836103f32ce9a34f7bf29b450ee5688
minjilee <budlalswl1129@gmail.com>
2022-09-20 01:58:11 +0900

    chore: 이미지 크롭 기능 수정

--- frontend/src/components/getCroppedImg.js
+++ frontend/src/components/getCroppedImg.js
@@ -1,24 +1,57 @@
-import { Area } from "react-easy-crop";
 export const createImage = (url) =>
   new Promise((resolve, reject) => {
     const image = new Image();
     image.addEventListener("load", () => resolve(image));
     image.addEventListener("error", (error) => reject(error));
-
     image.setAttribute("crossOrigin", "anonymous");
     image.src = url;
   });
 
-export default async function getCroppedImg(imageSrc, pixelCrop) {
+export function getRadianAngle(degreeValue) {
+  return (degreeValue * Math.PI) / 180;
+}
+
+export function rotateSize(width, height, rotation) {
+  const rotRad = getRadianAngle(rotation);
+
+  return {
+    width:
+      Math.abs(Math.cos(rotRad) * width) + Math.abs(Math.sin(rotRad) * height),
+    height:
+      Math.abs(Math.sin(rotRad) * width) + Math.abs(Math.cos(rotRad) * height),
+  };
+}
+
+export default async function getCroppedImg(
+  imageSrc,
+  pixelCrop,
+  rotation = 0,
+  flip = { horizontal: false, vertical: false }
+) {
   const image = await createImage(imageSrc);
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
+
   if (!ctx) {
-    return "";
+    return null;
   }
 
-  canvas.width = image.width;
-  canvas.height = image.height;
+  const rotRad = getRadianAngle(rotation);
+
+  const { width: bBoxWidth, height: bBoxHeight } = rotateSize(
+    image.width,
+    image.height,
+    rotation
+  );
+
+  // set canvas size to match the bounding box
+  canvas.width = bBoxWidth;
+  canvas.height = bBoxHeight;
+
+  ctx.translate(bBoxWidth / 2, bBoxHeight / 2);
+  ctx.rotate(rotRad);
+  ctx.scale(flip.horizontal ? -1 : 1, flip.vertical ? -1 : 1);
+  ctx.translate(-image.width / 2, -image.height / 2);
 
   ctx.drawImage(image, 0, 0);
 
@@ -36,7 +69,7 @@ export default async function getCroppedImg(imageSrc, pixelCrop) {
 
   return new Promise((resolve, reject) => {
     canvas.toBlob((file) => {
-      if (file !== null) resolve(URL.createObjectURL(file));
+      resolve(URL.createObjectURL(file));
     }, "image/jpeg");
   });
 }
