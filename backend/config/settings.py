cec880b2925382824bb21d504c6ce5ecd02b692e
Heekyeong Park <qkrgmlrud00@gmail.com>
2022-09-25 02:24:58 +0900

    error: celery docker setting 에러

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -208,3 +208,13 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Asia/Seoul'
+
+# RABBITMQ = {
+#     "PROTOCOL": "amqp",  # in prod change with "amqps"
+#     "HOST": os.getenv("RABBITMQ_HOST", "localhost"),
+#     "PORT": os.getenv("RABBITMQ_PORT", 5672),
+#     "USER": os.getenv("RABBITMQ_USER", "guest"),
+#     "PASSWORD": os.getenv("RABBITMQ_PASSWORD", "guest"),
+# }
+#
+# CELERY_BROKER_URL = f"{RABBITMQ['PROTOCOL']}://{RABBITMQ['USER']}:{RABBITMQ['PASSWORD']}@{RABBITMQ['HOST']}:{RABBITMQ['PORT']}"
--- backend/photoon/tasks.py
+++ backend/photoon/tasks.py
@@ -6,6 +6,7 @@ import time
 @app.task
 def longtime_add(x, y):
     print('long time task begins')
+
     # sleep 5 seconds
     time.sleep(5)
     print('long time task finished')
