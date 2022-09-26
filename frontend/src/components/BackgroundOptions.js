0e8e547cad50ede4772f6ada7b001e56f0b57f2d
KoneJ <kbvv67890@gmail.com>
2022-09-27 03:53:22 +0900

    fix : 옵션 선택 통신 1차

--- frontend/src/components/BackgroundOptions.js
+++ frontend/src/components/BackgroundOptions.js
@@ -1,20 +1,47 @@
 import React, { useState } from "react";
 import "./Options.css";
 import MoveButton from "../components/MoveButton";
-export default function CartoonOptions() {
-  const [x, setX] = useState([]);
-  const backgroundsKind = [
-    { name: "1", img: "images/ice-2.jpg" },
-    { name: "2", img: "images/ice-2.jpg" },
-    { name: "3", img: "images/ice-2.jpg" },
-  ];
+import { useSelectContentStore } from "../components/store";
+import axios from 'axios';
 
+
+export default function CartoonOptions() {
+
+  const { style , background , setBackground} = useSelectContentStore(); //zustand 전역변수
+
+  const backgroundsKind = [
+    { name: 1, img: "images/ice-2.jpg" },
+    { name: 2, img: "images/ice-2.jpg" },
+    { name: 3, img: "images/ice-2.jpg" },
+  ];
+  
   const handleClickRadioButton = (e) => {
-    sessionStorage.setItem('background', JSON.stringify({"background": e.target.value}));
-    JSON.parse(localStorage.getItem('json'));
+    console.log()
     console.log(e.target.value);
-    setX(e.target.value);
+    setBackground(e.target.value);
   };
+  
+  const formData = new FormData()
+  formData.append('style', style)
+  formData.append('background', 'background')
+  
+
+  const options = () => {
+    
+    axios.post('http://127.0.0.1:8000/api/v1/results/', formData, 
+    )
+    
+
+  .then(response => {
+    // Handle success.
+    console.log('만화 선택 옵션', response.data.style);
+    console.log('배경 선택 옵션', response.data.background);
+  })
+  .catch(error => {
+    // Handle error.
+    console.log('An error occurred:', error.response);
+  });
+    }
 
   return (
     <div style={{ marginRight: "6px" }}>
@@ -34,20 +61,19 @@ export default function CartoonOptions() {
               type="radio"
               className="input-hidden"
               value={kind.name}
-              checked={x === `${kind.name}`}
+              checked={background === `${kind.name}`}
               onChange={handleClickRadioButton}
             />
             <img src={kind.img} className="Image" alt="배경분리" />
           </label>
         ))}
       </div>
-      <MoveButton
+      <button onClick={() => {
+        options();
+        }}>asdfasdfafs</button>
+        
+        
 
-        url1="/choicecartoon"
-        url2="/result"
-        style1="w-12"
-        style2="w-12"
-      />
     </div>
   );
 }
